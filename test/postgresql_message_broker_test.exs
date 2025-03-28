cbe429d608a3d22ffcf179b35d7aa0f3e0d4e2b3
Paul Dann <pgdann@gmail.com>
2025-03-28 11:59:03 +0000

    Configure tests to be next to the code

--- test/postgresql_message_broker_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule PostgresqlMessageBrokerTest do
-  use ExUnit.Case
-  doctest PostgresqlMessageBroker
-
-  test "greets the world" do
-    assert PostgresqlMessageBroker.hello() == :world
-  end
-end
